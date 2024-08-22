bb450076126ab15920604e61f37ddc7d8607e694
Frontesg <155605726+Frontesg@users.noreply.github.com>
2024-08-22 00:10:17 -0300

    Delete screenshot.png

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ import cv2
 
 
 # Função para capturar a tela ao redor do ponteiro do mouse
-def capture_screenshot_around_mouse(region_size=300):
+def capture_screenshot_around_mouse(region_size=250):
     # Obtém a posição atual do mouse
     x, y = pyautogui.position()
 
     # Calcula as coordenadas do retângulo em volta do mouse
     left = x - region_size // 1
-    top = y - region_size // 2
-    right = x + region_size // 1
-    bottom = y + region_size // 2
+    top = y - region_size // 3.5
+    right = x # + region_size // 1
+    bottom = y # + region_size // 2
 
     # Captura a região da tela em volta do mouse
     screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
@@ -31,8 +31,12 @@ def capture_screenshot_around_mouse(region_size=300):
     imagem = cv2.imread("screenshot.png")
     pyt.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
     texto = pyt.image_to_string(imagem, lang="por")
+    textoT1 = texto.find('<')
+    textoT2 = texto[textoT1+1:].split()
+    textoT3 = textoT2[0]
 
-    print(texto)
+    
+    print('Mapa: '+ textoT3)
 
     #print(f"Screenshot capturado e salvo como screenshot_{timestamp}.png")
 
@@ -46,3 +50,14 @@ def listen_for_screenshot():
 
 # Inicia a escuta do atalho de teclado
 listen_for_screenshot()
+'''
+O
+
+Estrada de Avalon para
+
+<& Tynos-Eyexroi
+
+2577 10239
+
+XErFechoem 2h38m
+'''
