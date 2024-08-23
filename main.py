50431866fbbbb54c133438ab0fb217ab5d390446
Frontesg <frontes.dev@gmail.com>
2024-08-22 21:49:56 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import pytesseract as pyt
 import cv2
 
 
-# Função para capturar a tela ao redor do ponteiro do mouse
+# Função para capturar a tela
 def capture_screenshot_around_mouse(region_size=250):
     # Obtém a posição atual do mouse
     x, y = pyautogui.position()
 
-    # Calcula as coordenadas do retângulo em volta do mouse
+    # coordenadas
     left = x - region_size // 1
     top = y - region_size // 3.5
     right = x # + region_size // 1
@@ -24,8 +24,7 @@ def capture_screenshot_around_mouse(region_size=250):
     # Captura a região da tela em volta do mouse
     screenshot = ImageGrab.grab(bbox=(left, top, right, bottom))
 
-    # Salva o screenshot com um nome único
-    #timestamp = int(time.time())
+    # Salva o screenshot.
     screenshot.save(f"screenshot.png")
 
     imagem = cv2.imread("screenshot.png")
@@ -38,26 +37,13 @@ def capture_screenshot_around_mouse(region_size=250):
     
     print('Mapa: '+ textoT3)
 
-    #print(f"Screenshot capturado e salvo como screenshot_{timestamp}.png")
-
 # Função que aguarda o atalho Ctrl + X
 def listen_for_screenshot():
-    print("Pressione Ctrl + X para capturar o screenshot ao redor do mouse.")
+    print("Pressione Ctrl + X para capturar o screenshot.")
     while True:
         if keyboard.is_pressed('ctrl+x'):
             capture_screenshot_around_mouse()
-            time.sleep(1)  # Evita múltiplos prints com um único pressionamento de tecla
+            time.sleep(1)  # Evita múltiplos prints
 
 # Inicia a escuta do atalho de teclado
 listen_for_screenshot()
-'''
-O
-
-Estrada de Avalon para
-
-<& Tynos-Eyexroi
-
-2577 10239
-
-XErFechoem 2h38m
-'''
