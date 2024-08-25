6ef969e790ed04b8572768f0a0b75594e43daa41
Frontesg <frontes.dev@gmail.com>
2024-08-25 18:41:16 -0300

    modified:   models/repository/mapa_info_repository/mapInfo_repository.py

--- models/repository/mapa_info_repository/mapInfo_repository.py
+++ models/repository/mapa_info_repository/mapInfo_repository.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+from bson.objectid import ObjectId
 
 class MapInfoRepository:
     def __init__(self, db_connection) -> None:
@@ -18,15 +19,40 @@ class MapInfoRepository:
 #Busca
     def select_many(self, filter) -> List[Dict]:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        data = collection.find(
-            filter,
-            { "_id": 0 } # Remove parametro
-        )
+        data = collection.find(filter, {"_id": 0}) # Remove parametro
         response = []
         for elem in data: response.append(elem)
         return response
     
     def select_one(self, filter) -> Dict:
         collection = self.__db_connection.get_collection(self.__collection_name)
-        response = collection.find_one(filter, { "id": 0 })
+        response = collection.find_one(filter, {"id": 0})
+        return response
+    
+    def select_if_property_existis(self, filter) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find( {filter: {"$exists": True}}, {"id" :0})
+        response = []
+        for elem in data: response.append(elem)
+        return response
+    
+    def select_many_order(self, filter, filter2) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find(filter, {"_id": 0}).sort([(filter2)]) # filter2 exem: ()"pedidos.pizza", 1)
+        response = []
+        for elem in data: response.append(elem)
+        return response
+    
+    def select_or(self, filter) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find({ "$or": [filter] })
+        response = []
+        for elem in data: response.append(elem)
+        return response
+    
+    def select_by_object_id(self, id) -> List[Dict]:
+        collection = self.__db_connection.get_collection(self.__collection_name)
+        data = collection.find({ "_id": ObjectId(id) })
+        response = []
+        for elem in data: response.append(elem)
         return response
