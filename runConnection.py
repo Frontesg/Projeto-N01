606adc2d10215ac03bd94f280004fbccd4aa94d9
Frontesg <frontes.dev@gmail.com>
2024-08-25 13:09:10 -0300

    Definindo estrutura

---
+++ runConnection.py
@@ -0,0 +1,7 @@
+from models.connection_options.connection import DBConnectionHandler
+
+db_handle = DBConnectionHandler()
+conn1 = db_handle.get_db_connection()
+
+db_handle.connect_to_db()
+conn2 = db_handle.get_db_connection()
